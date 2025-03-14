104d7c5c344a597dfb706073ae2817c979dbc898
Hirtik Malvi <hirtik.malvi@gmail.com>
2025-03-14 23:45:21 +0530

    Setup Prisma, PostgreSQL (Neon DB) In Backend

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,19 @@
-import express from "express";
+import express, { json } from "express";
+import { PrismaClient, Prisma } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/", (req, res) => {
-  res.send("<h1>CivikVoice</h1>");
+app.get("/", async (req, res) => {
+  const users = await prisma.users.findMany();
+  res.json(
+    JSON.stringify(
+      users.map((user) => ({
+        ...user,
+        user_id: user.user_id.toString(),
+      }))
+    )
+  );
 });
 
 app.listen(5000, () => console.log("Working Fine"));
