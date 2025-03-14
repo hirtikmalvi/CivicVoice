38a9073d91b84f4b14eb8bbc1050a95b9d3978c5
Parth Chavda <parthchavda159@gmail.com>
2025-03-14 18:24:58 +0530

    setup for render deployment

--- backend/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("<h1>CivikVoice</h1>");
-});
-
-app.listen(5000, () => console.log("Working Fine"));
